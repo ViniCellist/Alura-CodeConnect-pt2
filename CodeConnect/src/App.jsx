64d576e3da9013a7f26076d5ae25a3a8c5251e41
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-10-09 15:54:55 -0300

    div component than fragmnent

--- CodeConnect/src/App.jsx
+++ CodeConnect/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import BarraPesquisa from './components/BarraPesquisa'
+import Filtro from './components/Filtro'
 import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
-    <>
+    <div className='container'>
       <Sidebar />
-    </>
+      <div>
+        <BarraPesquisa />
+        <Filtro />
+      </div>
+    </div>
   )
 }
 
