fb727dcb24870d3e33c9ffb4c4ad8fb4c2929676
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-10-09 15:55:28 -0300

    filtro component e css

---
+++ CodeConnect/src/components/Filtro/index.jsx
@@ -0,0 +1,14 @@
+import "./Filtro.css";
+
+const Filtro = () => {
+    return (
+        <section className="containerFiltro">
+            <ul>
+                <li>Programação</li>
+            </ul>
+            <button>Limpar tudo</button>
+        </section>
+    );
+};
+
+export default Filtro;
