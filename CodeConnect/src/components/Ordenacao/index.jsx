d58ef4ba554f9d52153be9aa016203b43a9765bd
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-10-10 17:58:16 -0300

    feat ordenacao component

---
+++ CodeConnect/src/components/Ordenacao/index.jsx
@@ -0,0 +1,16 @@
+import "./Ordenacao.css";
+
+const Ordenacao = () => {
+    return (
+        <ul className="lisaOrdenacao">
+            <li>
+                <a href="" className="listaOrdenacaoLink listaOrdenacaoLink-ativo">Recentes</a>
+            </li>
+            <li>
+                <a href="" className="listaOrdenacaoLink">Recentes</a>
+            </li>
+        </ul>
+    );
+};
+
+export default Ordenacao;
