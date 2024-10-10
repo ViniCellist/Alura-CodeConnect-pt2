61bf268776c3330677e0f5181e4ded9c5ac6759d
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-10-10 17:57:47 -0300

    feat api consume

--- CodeConnect/src/App.jsx
+++ CodeConnect/src/App.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import BarraPesquisa from './components/BarraPesquisa'
 import Filtro from './components/Filtro'
 import Sidebar from './components/Sidebar'
+import Card from './components/Card/index';
+import Ordenacao from './components/Ordenacao/index';
 
 function App() {
+
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes")
+    .then(res => res.json())
+    .then(dados => setDados(dados))
+  }, [])
 
   return (
     <div className='container'>
@@ -11,6 +21,23 @@ function App() {
       <div>
         <BarraPesquisa />
         <Filtro />
+        <Ordenacao />
+        <ul className="listaCards">
+            {dados ? dados.map((item, index) => (
+              <li key={index}>
+                  <Card 
+                    id={item.id}
+                    imgUrl={item.imagem_capa}
+                    titulo={item.titulo}
+                    resumo={item.resumo}
+                    linhasDeCodigo={item.linhas_de_codigo}
+                    compartilhamentos={item.compartilhamentos}
+                    comentarios={item.comentarios}
+                    usuario={item.usuario}
+                  />
+              </li>
+            )) : null}
+        </ul>
       </div>
     </div>
   )
